07b1cb18b68b8adecfaa222939ee9f6f8817c3dd
Inna Kryvdiuk <82314667+QAInnaQA@users.noreply.github.com>
2023-12-29 21:09:56 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,85 +3,77 @@ import { data } from "./data";
 import './App.css';
 
 function App() {
-  //если хочу отобразить первый элемент, то нужно указать useState(0)?
-  const [movies, setMovies] = useState(data); 
-  const [showText, setShowText] = useState(false)
-
+  const [movies, setMovies] = useState(0);
+  const [showText, setShowText] = useState(false);
+  const [list, setList] = useState(data);
+  const {id, nameMovie, year, image, synopsis} = data[movies];
 
   const removeMovie = (id) => {
-    let newMovies = movies.filter(movie => movie.id !==id);
-    setMovies(newMovies)
+    let newMovies = list.filter(movie => movie.id !== id);
+    setList(newMovies)
   }
 
-  const showTextClick = (element) => {
-    element.showMore = !element.showMore
-    setShowText(!showText)
+  const previousMovie = () => {
+    setMovies((movie => {
+      movie--;
+      if (movie < 0) {
+        return data.length - 1;
+      }
+      return movie;
+    }))
   }
 
-  // const previousMovie = () => {
-  //   setMovie((movie => {
-  //     movie --;
-  //     if (movie < 0) {
-  //       return data.length-1;
-  //     }
-  //     return movie;
-  //   }))
-  // }
+  const nextMovie = () => {
+    setMovies((movie => {
+      movie++;
+      if (movie > data.length - 1) {
+        movie = 0;
+      }
+      return movie;
+    }))
+  }
 
-  // const nextMovie = () => {
-  //   setMovie((movie =>{
-  //     movie ++;
-  //     if (movie > data.length -1) {
-  //       movie = 0;
-  //     }
-  //     return movie;
-  //   }))
-  // }
-
-  return(
-<div>
-
-  <div className="container">
-    <h1>THE BEST {movies.length} CHRISTMAS MOVIES OF ALL TIME</h1>
-  </div>
-
-  {movies.map((element => {
-    // если хочу отображать слайдами, то нужно указать element[nameMovie]?
-    const {id, nameMovie, year, image, synopsis, showMore} = element;
-
-    return(<div key={id}>
+  return (
+    <div>
 
       <div className="container">
-        <h2>{id}. {nameMovie}</h2>
+        <h1>THE BEST {list.length} CHRISTMAS MOVIES OF ALL TIME</h1>
+      </div>
+
+      <div>
+        <div className="container">
+          <h2>{id}. {list[movies].nameMovie}</h2>
+        </div>
+
+        <div className="container">
+          <h2>{list[movies].year}</h2>
+        </div>
+
+        <div className="container">
+          <button className="btn previous" onClick={previousMovie}>Previous</button>
+          <img src={list[movies].image} width="250px" alt="movie" />
+          <button className="btn next" onClick={nextMovie}>Next</button>
+        </div>
+
+        <div className="container">
+          <button className="btn" onClick={() => removeMovie(list[movies].id)}>Remove</button>
+        </div>
+
+        <div className="container">
+          <p>{showText ? list[movies].synopsis : list[movies].synopsis.substring(0, 100) + "..."}
+            <button className="btnMore" onClick={() => setShowText(!showText)}>
+              {showText ? "less" : "more"}
+            </button>
+          </p>
+        </div>
+
       </div>
 
       <div className="container">
-        <h2>{year}</h2>
+        <button className="btn" onClick={() => list([])}>REMOVE ALL</button>
       </div>
 
-      <div className="container">
-      {/* <button className="btn previous" onClick={previousMovie}>Previous</button> */}
-        <img src={image} width="250px" alt="movie"/>
-      {/* <button className="btn next" onClick={nextMovie}>Next</button> */}
-      </div>
-
-      <div className="container">
-        <button className="btn" onClick={() => removeMovie(id)}>Remove</button>
-      </div>
-
-      <div className="container">
-        <p>{showMore ? synopsis : synopsis.substring(0,100) + "..."}
-        <button className="btnMore" onClick={() => showTextClick(element)}>{showMore ? "less" : "more"}</button>
-        </p>
-      </div>
-
-    </div>)
-  }))}
-      <div className="container">
-        <button className="btn" onClick={() => setMovies([])}>REMOVE ALL</button>
-      </div>
-
-</div>
+    </div>
 
   )
 
